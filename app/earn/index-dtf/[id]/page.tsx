eebadd6cf935d48dc215127186bdf0a74e62027f
Josh <webposer@gmail.com>
2026-02-25 17:29:13 -0700

    routing

--- app/earn/index-dtf/[id]/page.tsx
+++ app/earn/index-dtf/[id]/page.tsx
@@ -1,7 +1,13 @@
 import DtfDetailClient from "./DtfDetailClient"
 
 export function generateStaticParams() {
-  return [{ id: "bgci" }]
+  return [
+    { id: "baix" },
+    { id: "bex" },
+    { id: "mvtt10f" },
+    { id: "bgci" },
+    { id: "dgi" },
+  ]
 }
 
 export default function Page() {
